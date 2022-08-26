be2ed5e4d43111cd7342555fc5fbb4b3af12521d
pablopachecoo <53284072+pablopachecoo@users.noreply.github.com>
2022-08-25 23:01:47 -0300

    JDBC + Connection Pool + Flyway + JDBC Template + SQL + Docker

---
+++ src/main/java/com/amigoscode/movie/MovieDao.java
@@ -0,0 +1,15 @@
+package com.amigoscode.movie;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MovieDao {
+    List<Movie> selectMovies();
+    @Transactional
+    int insertMovie(Movie movie);
+    int deleteMovie(int id);
+    Optional<Movie> selectMovieById(int id);
+    // TODO: Update
+}
